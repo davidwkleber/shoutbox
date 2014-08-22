636448460c1b98613d13f10f484c13cb77a78163
davidwkleber <david.kleber@gmail.com>
2014-08-22 16:42:14 +0200

    now it has the post and page

--- app.js
+++ app.js
@@ -10,11 +10,26 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
+// used for registration page
 var register = require('./routes/register');
 var messages = require('./lib/messages');
 
 
+// used for login page
+var login = require('./routes/login');
+
+// used for keeping user id in session
+var user = require('./lib/middleware/user');
+
+// used for posting messages.
+var entries = require('./routes/entries');
+
+// used for post input validation
+var validate = require('./lib/middleware/validate');
+
+// for paging
+var page = require('./lib/middleware/page');
+var Entry = require('./lib/entry');
 var app = express();
 
 app.disable('etag');
@@ -30,16 +45,35 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({secret: 'keyboard cat', resave: 'true', saveUninitialized: 'true' }));
- app.use(messages);
+app.use(user);
+app.use(messages);
+
+// app.use(app.router);
 
 
-app.use('/', routes);
- app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
 
+// have / return the entries list
+// app.get('/', page(Entry.count, 5), entries.list); 
+
+// routes for registration page
 app.get('/register', register.form);
 app.post('/register', register.submit);
 
+// routes for login page
+app.get('/login', login.form);
+app.post('/login', login.submit);
+app.get('/logout', login.logout);
 
+// for listing posts
+app.get('/post', entries.form);
+app.post('/post',
+	validate.required('entry[title]'),
+	validate.lengthAbove('entry[title]', 4),
+	entries.submit);
+	
+app.get('/:page?', page(Entry.count, 5), entries.list);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
